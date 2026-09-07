65d11dfa2e6b2a5f4a71eac944519bf0395062e2
Tim Ohling <trohling@peak.org>
2026-09-07 15:09:51 -0700

    baseline working with kPosition

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+ 
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static final class armConstants {
+    public static final int motorArm = 30;
+    public static final double kGearRatio = 5.0 * 3.0 * 32.0 / 18.0;
+    public static final double kEncoderDegrees = 360 / kGearRatio;
+    public static final double kArmUp = 85; // / kEncoderDegrees; // angle is negative degrees
+    public static final double kArmDown = 170; // / kEncoderDegrees;
+    public static final int kCurrentLimit = 20;
+    public static final double kSecondaryCurrentLimit = 30;
+    public static final double kPostionAdjust = 15  / kEncoderDegrees; // angle is in degrees 
+    public static final double kFwdSoftLimit = 180 / kEncoderDegrees;
+    public static final double kRevSoftLimit = 80 / kEncoderDegrees;
+    //public static final double kHandDownish = 105; // for absolute encoder actual degrees
+
+ }
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,132 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.BooleanSupplier;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.config.FeedForwardConfig;
+import com.revrobotics.spark.config.SoftLimitConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.armConstants;
+
+public class ArmSubsystem extends SubsystemBase {
+  /** Creates a new ArmSubsystem. */
+  private SparkMax m_motorArm;
+  private SparkMaxConfig m_motorConfigArm;
+  //private FeedForwardConfig m_armFeedForwardConfig;
+  private RelativeEncoder m_encoderArm;
+  private AbsoluteEncoder m_absEncoderArm;
+  private SparkClosedLoopController m_closedLoopArm;
+  private SoftLimitConfig m_SoftLimitArm;
+  private double m_ArmTargetPosition = armConstants.kArmUp;
+
+  public ArmSubsystem() {
+    m_motorArm = new SparkMax(armConstants.motorArm, MotorType.kBrushless);
+    m_encoderArm = m_motorArm.getEncoder();
+    m_absEncoderArm = m_motorArm.getAbsoluteEncoder();
+    m_motorConfigArm = new SparkMaxConfig();
+    m_motorConfigArm.idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(armConstants.kCurrentLimit)
+        .secondaryCurrentLimit(armConstants.kSecondaryCurrentLimit);
+    m_motorConfigArm.closedLoop
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        .p(0.005)
+        .i(0)
+        .d(0.04)
+        .outputRange(-.15, .15);
+    m_motorConfigArm.closedLoop.feedForward
+      .kCos(0.13)
+      .kS(0.01);
+    
+    //m_motorConfigArm.closedLoop.maxMotion
+    //    .maxAcceleration(1000)
+    //    .cruiseVelocity(2000)
+    //    .allowedProfileError(1);
+    m_closedLoopArm = m_motorArm.getClosedLoopController();
+    
+    m_motorConfigArm.absoluteEncoder
+        .inverted(false)
+        .positionConversionFactor(360);
+    //m_SoftLimitArm = new SoftLimitConfig();
+    //m_SoftLimitArm.forwardSoftLimit(armConstants.kFwdSoftLimit)
+    //    .forwardSoftLimitEnabled(true)
+    //    .reverseSoftLimit(armConstants.kRevSoftLimit)
+    //    .reverseSoftLimitEnabled(true);
+    //m_motorConfigArm.apply(m_SoftLimitArm);
+
+    m_motorArm.configure(m_motorConfigArm, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    setRelativeEncoder();
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Arm Postion", m_encoderArm.getPosition());
+    SmartDashboard.putNumber("Arm Target", m_ArmTargetPosition);
+    SmartDashboard.putNumber("Arm Abs Enc", m_absEncoderArm.getPosition());
+    SmartDashboard.putNumber("Arm Current", m_motorArm.getOutputCurrent());
+  }
+
+  public void stopArmMotor() {
+    m_motorArm.set(0);
+  }
+
+  public Command cmdStopArm() {
+    return Commands.runOnce(() -> stopArmMotor(), this);
+  }
+
+  private void adjustArmPosition(boolean isAdjustUp) {
+    setRelativeEncoder();
+    if (isAdjustUp) {
+      m_ArmTargetPosition = m_ArmTargetPosition + armConstants.kPostionAdjust;
+    } else {
+      m_ArmTargetPosition = m_ArmTargetPosition - armConstants.kPostionAdjust;
+    }
+    // if (m_ArmTargetPosition > armConstants.kFwdSoftLimit) {
+    // m_ArmTargetPosition = armConstants.kFwdSoftLimit}
+    // etc
+    m_closedLoopArm.setSetpoint(m_ArmTargetPosition, ControlType.kPosition);
+  }
+
+  private void setArmPosition(double targetPosition) {
+    setRelativeEncoder();
+    m_ArmTargetPosition = targetPosition;
+    m_closedLoopArm.setSetpoint(m_ArmTargetPosition, ControlType.kPosition);
+  }
+
+  public Command cmdSetArmUp() {
+    return Commands.runOnce(() -> setArmPosition(armConstants.kArmUp), this);
+  }
+
+  public Command cmdSetArmDown() {
+    return Commands.runOnce(() -> setArmPosition(armConstants.kArmDown), this);
+  }
+
+  public Command cmdAdjustArmPosition(boolean isAdjustUp) {
+    return Commands.runOnce(() -> adjustArmPosition(isAdjustUp), this);
+  }
+
+  private void setRelativeEncoder() {
+    m_encoderArm.setPosition(m_absEncoderArm.getPosition() / armConstants.kEncoderDegrees);
+  }
+}
